2c1ce0b136fbfa078e25d8562b18d366a19feb6b
alibarmonster <alibar838@gmail.com>
2026-01-04 21:24:15 +0700

    feat: update contract address

--- frontend/src/lib/contracts/addresses.ts
+++ frontend/src/lib/contracts/addresses.ts
@@ -5,7 +5,7 @@
  * forge script script/DeployMock.s.sol --rpc-url <RPC_URL> --broadcast
  */
 export const contractAddress = {
-  NinahContractAddress: '0x', // Update after deployment
-  MockIDRXAddress: '0x', // Update after deployment
-  SP1VerifierAddress: '0x', // MockSP1Verifier - Update after deployment
+  NinahContractAddress: "0x3c2d3963500945e6596f7ebead989bd927597251", // Update after deployment
+  MockIDRXAddress: "0x31f4f8b4bc2041a9848bb6baf9045e5e346fa72a", // Update after deployment
+  SP1VerifierAddress: "0xc7A036cAD9d61411e589C57C917DFeC684f02eC8", // MockSP1Verifier - Update after deployment
 };
